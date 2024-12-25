914256e8e1883646d01a1e8e5b4ef0cd5cfd4efa
connectwp <150893902+connectwp@users.noreply.github.com>
2024-12-25 13:51:34 +0500

    added critical plugins one-time-login and wp-rollback

--- wp/wp-content/mu-plugins/wpsp-critical-plugins-loader.php
+++ wp/wp-content/mu-plugins/wpsp-critical-plugins-loader.php
@@ -12,20 +12,24 @@ Author URI: https://wpscalepro.com/
 License: GPLv2 or later
 */
 
-//cdn-enabler - CDN urls are hard coded. To disable the CDN simply comment out the following line.
+
+//#1 display some important info at wp admin bar you may want to disable it.
+require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/wpsp-display-imp-info-at-admin-bar/wpsp-display-imp-info-at-admin-bar.php' );
+
+//#2 cdn-enabler - CDN urls are hard coded. To disable the CDN simply comment out the following line.
 require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/cdn-enabler/cdn-enabler.php' );
 
-//redis-cache - Cache is hard coded. To disable the CDN simply comment out the following line.
+//#3 redis-cache - Cache is hard coded. To disable the CDN simply comment out the following line.
 require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/redis-cache/redis-cache.php' );
 
-//wp-rollback - 
+//#4 wp-rollback - 
 define( 'WP_ROLLBACK_PLUGIN_FILE', '/mnt/network-share/wp-content/site'.WPSP_SITE_ID.'/mu-plugins/wpsp-critical-plugins/wp-rollback/wp-rollback/' );
 require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/wp-rollback/wp-rollback.php' );
 
 
-//display some important info at wp admin bar.
-require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/wpsp-display-imp-info-at-admin-bar/wpsp-display-imp-info-at-admin-bar.php' );
 
+//#5 one-time-login - critical plugin. Do not Remove or Comment out.*****
+require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/one-time-login/one-time-login.php' );
 
-//WPScalePro critical plugin. Do not Remove or Comment out.*****
+//#6 WPScalePro - critical plugin. Do not Remove or Comment out.*****
 require_once( '/var/www/WPScalePro/wp/wp-content/mu-plugins/wpsp-critical-plugins/wpsp-git-cicd-magic-deploy/wpsp-git-cicd-magic-deploy.php' );
